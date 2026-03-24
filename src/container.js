3fdf36050eaadb89b0039741becf7ff50b84929d
Dolfi-er <dolgovasofia@yandex.ru>
2026-03-24 11:40:26 +0300

    container

---
+++ src/container.js
@@ -0,0 +1,33 @@
+export class Container {
+    constructor(){
+        this._factories = new Map(); //ключ -> {kind, factory}
+        this._singletons = new Map(); // кэш синглтонов
+    }
+
+    //регистрация службы как синглтон
+    addSingleton(key, factory){
+        this._factories.set(key,{kind:'singleton', factory});
+    }
+
+    //регистрация службы как transient
+    addTransient(key, factory){
+        this._factories.set(key,{kind:'transient', factory});
+    }
+
+    //получение экземпляра службы по ключу
+    get(key){
+        const entry = this._factories.get(key);
+        if(!entry){
+            throw new Error(`Service ${key} not registered`);
+        }
+
+        if(entry.kind === 'singleton'){
+            if(!this._singletons.has(key)){
+                this._singletons.set(key, entry.factory(this));
+            }
+            return this._singletons.get(key);
+        }
+
+        return entry.factory(this);
+    }
+}
